03fcfc3de4b29d76892a09470d21cd60264494e6
philippe Pereira <phil.pereira@protonmail.com>
2020-10-11 11:55:33 +0200

    background color change randomly

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ document.getElementById( 'bouton' ).addEventListener( 'click', function()
     setInterval(function() {
         window.navigator.vibrate( 300 );
         buttonMove();
-    }, 50);
+    }, 500 );
 } );
 
 function buttonMove ()
@@ -25,5 +25,22 @@ function buttonMove ()
     {
         i = 0;
     }
-    
+    setInterval(function() {
+        window.navigator.vibrate( 300 );
+        changeBackground();
+    }, 400);
+}
+
+function changeBackground ()
+{
+    document.body.style.backgroundColor = getRandomColor();
+}
+
+function getRandomColor() {
+    var letters = "0123456789ABCDEF".split('');
+    var color = "#";
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
 }
