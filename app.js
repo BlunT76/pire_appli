a64b5cd3ba5cb57c07a71dc291f2ecc095863c42
philippe Pereira <phil.pereira@protonmail.com>
2020-10-11 11:44:00 +0200

    button can move

--- app.js
+++ app.js
@@ -1,5 +1,29 @@
-document.getElementById('bouton').addEventListener('click', function() {
+var i = 0;
+const btn = document.getElementById( 'bouton' );
+document.body.style.height = '100vh';
+
+document.getElementById( 'bouton' ).addEventListener( 'click', function()
+{
     setInterval(function() {
-        window.navigator.vibrate(300);
-    }, 500);
-});
+        window.navigator.vibrate( 300 );
+        buttonMove();
+    }, 50);
+} );
+
+function buttonMove ()
+{
+    
+    var num = Math.floor(Math.random()*99) + 1;
+    num *= Math.floor(Math.random()*2) == 1 ? 1 : -1;
+    i += num / 50 ;
+    
+    btn.style.position = "absolute";
+    if( i > 0 )
+    {
+        btn.style.top = i + 'px';
+    } else
+    {
+        i = 0;
+    }
+    
+}
